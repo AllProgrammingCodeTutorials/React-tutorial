1ca6da7f24dec5323eef337b1ac0f546525afc98
purnimagurung <grg.puru@gmail.com>
2021-09-24 16:17:18 +0200

    navbar menu is done

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,21 +4,17 @@ import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Contact from "./components/Contact";
 function App() {
-    return (
-        <Router>
-            <Navbar/>
-          <div>
-            <Switch>
-            <Route path="/">
-                <Home />
-              </Route>
-              <Route path="/contact">
-                <Contact />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      );
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact}/>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- react-project/src/components/Home.js
+++ react-project/src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Pic from "../assets/g.jpg";
 
 export const Home = () => {
     return (
         <div>
+            <img src={Pic} alt="" srcset="" />
             <h1> my name is hello</h1>
         </div>
+        
     )
 }
 export default Home;
