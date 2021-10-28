255e970b302c21d56ab54f732640e755d3316c17
purnimagurung <grg.puru@gmail.com>
2021-10-28 16:43:14 +0200

    form handling in react js

--- react-project/src/components/Contact.js
+++ react-project/src/components/Contact.js
@@ -2,16 +2,28 @@ import React from 'react';
 import './Contact.css';
 
 class Contact extends React.Component {
+
+    constructor(){
+        super();
+        this.state ={
+            username:null,
+            email:null
+        }
+    }
+
+    formSubmit(){
+        console.log(this.state);
+    }
     render() {
         return(
             <div className="contact-form">
                 <h2> Contact Me</h2>
                 <form className ="ui form">
                     <label>username: </label>
-                    <input type="text" name="username"/><br/>
+                    <input type="text" name="username" onChange={(e)=>this.setState({username:e.target.value})} /><br/>
                     <label>Email: </label>
-                    <input type="text" name="email"/> <br/>
-                    <button className="btn">Submit</button>
+                    <input type="text" name="email" onChange={(e)=>this.setState({email:e.target.value})}/> <br/>
+                    <button className="btn" onClick={()=>this.formSubmit()}>Submit</button>
                 </form>
             </div>
         );
